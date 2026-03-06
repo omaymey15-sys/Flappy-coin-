87691d69cfbd72fae9c9b768c638afaf0fd0469c
omaymey15-sys <omaymey15@gmail.com>
2026-03-06 10:44:42 +0200

    Create LeaderboardActivity.kt

---
+++ app/src/main/java/com/example/flappycoin/activities/LeaderboardActivity.kt
@@ -0,0 +1,36 @@
+package com.example.flappycoin.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.flappycoin.databinding.ActivityLeaderboardBinding
+import com.example.flappycoin.managers.GamePreferences
+
+class LeaderboardActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLeaderboardBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLeaderboardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val bestScore = GamePreferences.getBestScore()
+        val bestCoins = GamePreferences.getBestCoins()
+        val username = GamePreferences.getUsername()
+
+        binding.tvLeaderboard.text = """
+            🏆 TOP SCORES (LOCAL)
+            
+            👤 $username
+            Score: $bestScore
+            Pièces: $bestCoins
+            
+            (Sauvegarde locale)
+            
+            💡 Conseil: Partagez votre score!
+        """.trimIndent()
+
+        binding.btnBack.setOnClickListener {
+            finish()
+        }
+    }
+}
