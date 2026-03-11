5ab5ea3c50d2a9231c0cea183e70eab6fe45bda2
omaymey15-sys <omaymey15@gmail.com>
2026-03-11 13:04:29 +0200

    Update AppOpenManager.kt

--- app/src/main/java/com/example/flappycoin/managers/AppOpenManager.kt
+++ app/src/main/java/com/example/flappycoin/managers/AppOpenManager.kt
@@ -5,6 +5,7 @@ import android.app.Application
 import android.util.Log
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.appopen.AppOpenAd
+import com.example.flappycoin.utils.Constants  // ← Important !
 
 class AppOpenManager(private val application: Application) {
 
@@ -15,36 +16,33 @@ class AppOpenManager(private val application: Application) {
     private var appOpenAd: AppOpenAd? = null
     private var isLoading = false
 
-    // Charger l'AppOpenAd
     fun loadAd() {
         if (isLoading || appOpenAd != null) return
         isLoading = true
 
         AppOpenAd.load(
             application,
-            Constants.APP_OPEN_AD_UNIT_ID,
+            Constants.ADMOB_APP_ID, // ou Constants.APP_OPEN_AD_UNIT_ID si tu le rajoutes
             AdRequest.Builder().build(),
             AppOpenAd.APP_OPEN_AD_ORIENTATION_PORTRAIT,
             object : AppOpenAd.AppOpenAdLoadCallback() {
                 override fun onAdLoaded(ad: AppOpenAd) {
                     appOpenAd = ad
                     isLoading = false
-                    Log.d(TAG, "AppOpenAd chargée")
+                    Log.d(TAG, "AppOpen chargée")
                 }
 
                 override fun onAdFailedToLoad(loadAdError: com.google.android.gms.ads.LoadAdError) {
-                    Log.e(TAG, "Échec chargement AppOpenAd: ${loadAdError.message}")
+                    Log.e(TAG, "Échec AppOpen: ${loadAdError.message}")
                     isLoading = false
                 }
             }
         )
     }
 
-    // Afficher la pub si disponible
     fun showAd(activity: Activity) {
         appOpenAd?.show(activity)
         appOpenAd = null
-        loadAd() // Recharge pour la prochaine ouverture
-        Log.d(TAG, "AppOpenAd affichée")
+        loadAd()
     }
 }
